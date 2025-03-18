47e25892898addaa54c6ef919f65927a064a19d2
minju <s2303@e-mirim.hs.kr>
2025-03-18 09:59:29 +0900

    express 기본예제

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const express = require('express');
+
 const app = express();
-const PORT = 3000;
 
-// 루트 엔드포인트
-app.get('/', (req, res) => {
-    res.type('text/plain');
-    res.send('Hello, Node.js!');
+app.get('/',(req,res) => {
+    res.send('Hello World');
 });
 
-// 서버 실행
-server.listen(3000, () => {
-    console.log('서버가 http://localhost:3000 에서 실행 중입니다.');
-});
+app.listen(3000,()=>{
+    console.log('서버가 http://localhost:3000 에서 실행됩니다.');
+})
--- 문법/for.js
+++ 문법/for.js
@@ -1,5 +1,5 @@
 const letter = ['a','b','c','d']
-
+ 
 // 기본 for
 for(let i=0; i<letter.length; i++){
     console.log(letter[i]);
