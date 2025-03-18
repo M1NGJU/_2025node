e60bc08761608c5a8ae7023859dd9bcbb2985dbf
minju <s2303@e-mirim.hs.kr>
2025-03-18 10:22:04 +0900

    메소드 변경

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.get('/',(req,res) => {
+app.post('/riku',(req,res) => {
     res.send('Hello World');
 });
 
 app.listen(3000,()=>{
     console.log('서버가 http://localhost:3000 에서 실행됩니다.');
-})
+});
